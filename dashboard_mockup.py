5b471aea7813d54863ac7083930d098fe98153c0
Omartg04 <omar.tellez@socialinclusiontech.com>
2025-07-21 23:09:03 -0600

    Add files via upload

--- dashboard_mockup.py
+++ dashboard_mockup.py
@@ -511,7 +511,7 @@ with tab4:
         
 # --- CAMBIO DE ESTILO DEL MAPA (MÉTODO DIRECTO) ---
         st.pydeck_chart(pdk.Deck(
-            map_style="mapbox://styles/mapbox/light-v10",
+            map_style=mapbox://styles/mapbox/outdoors-v11,
             initial_view_state=view_state,
             layers=layers,
             # --- AÑADE ESTA LÍNEA CON TU CLAVE ---
@@ -593,7 +593,7 @@ with tab5:
         view_state = pdk.ViewState(latitude=casa_seleccionada_coords['lat'], longitude=casa_seleccionada_coords['lon'], zoom=14, pitch=50)
         
         st.pydeck_chart(pdk.Deck(
-            map_style="mapbox://styles/mapbox/dark-v10",
+            map_style="mapbox://styles/mapbox/outdoors-v11",
             initial_view_state=view_state,
             # --- CAMBIO IMPORTANTE: Dibujar la capa de la casa al final para que quede encima ---
             layers=[capa_demanda, capa_casa],
